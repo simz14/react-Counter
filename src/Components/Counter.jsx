e33efeca230db6a734c7cae5944f76ab9d659c75
Simona Zozuľaková <szozulakova@gmail.com>
2022-12-02 11:28:26 +0100

    countDown,reseteCount done

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+
   const countUp = () => {
     setCount((prev) => prev + 1);
   };
+  const countDown = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const countReset = () => {
+    setCount((prev) => 0);
+  };
+
   return (
     <div>
       <div>Counter</div>
       <div>{count}</div>
       <button onClick={countUp}>Count up</button>
-      <button onClick={() => console.log("Down")}>Count down</button>
+      <button onClick={countDown}>Count down</button>
+      <button onClick={countReset}>Reset</button>
     </div>
   );
 };
