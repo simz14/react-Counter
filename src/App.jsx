4b97839bccb361e35ee37a08ee0450702c767671
Simona Zozuľaková <szozulakova@gmail.com>
2022-12-02 12:55:50 +0100

    Finnished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import Counter from "./Components/Counter";
+import { useState } from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  min-height: 100vh;
+  padding: 0;
+  background-color: #1c1c1c;
+  color: white;
+`;
 
 function App() {
-  return <Counter />;
+  const [count, setCount] = useState(0);
+
+  const countUp = () => {
+    setCount((prev) => prev + 1);
+  };
+  const countDown = () => {
+    setCount((prev) => prev - 1);
+  };
+  const countReset = () => {
+    setCount((prev) => (prev = 0));
+  };
+
+  return (
+    <AppWrapper>
+      <Counter
+        count={count}
+        upFn={countUp}
+        downFn={countDown}
+        resetFn={countReset}
+      />
+    </AppWrapper>
+  );
 }
 
 export default App;
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,27 +1,46 @@
-import { useState } from "react";
+import styled from "styled-components";
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
+const ContentWrapper = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  gap: 20px;
+`;
+const Title = styled.h1`
+  margin: 0;
+  display: flex;
+  justify-content: center;
+`;
 
-  const countUp = () => {
-    setCount((prev) => prev + 1);
-  };
-  const countDown = () => {
-    setCount((prev) => prev - 1);
-  };
+const Button = styled.button`
+  background-color: #5e5df0;
+  border-radius: 5px;
+  border: none;
+  margin: 5px;
+  color: white;
+`;
+const ButtonWrapper = styled.div`
+  displa: flex;
+`;
 
-  const countReset = () => {
-    setCount((prev) => 0);
-  };
+const Count = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 20px;
+`;
 
+const Counter = ({ count, upFn, downFn, resetFn }) => {
   return (
-    <div>
-      <div>Counter</div>
-      <div>{count}</div>
-      <button onClick={countUp}>Count up</button>
-      <button onClick={countDown}>Count down</button>
-      <button onClick={countReset}>Reset</button>
-    </div>
+    <ContentWrapper>
+      <Title>Counter</Title>
+      <Count>{count}</Count>
+      <ButtonWrapper>
+        <Button onClick={upFn}>Count up</Button>
+        <Button onClick={downFn}>Count down</Button>
+        <Button onClick={resetFn}>Reset</Button>
+      </ButtonWrapper>
+    </ContentWrapper>
   );
 };
 
